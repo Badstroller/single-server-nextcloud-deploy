6cb736a167bd0d75ae2495dc1ddec75303fc9bae
Badstroller <spagnolodavid@gmail.com>
2023-12-20 14:02:57 -0600

    Update script to allow for manual entry of ip address or hostname

--- nextcloud.sh
+++ nextcloud.sh
@@ -13,7 +13,13 @@ export ADMIN_PASSWORD=$admin_password
 mkdir ./deployment
 touch ./deployment/redis-session.ini
 
-export ip4=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+# Ask for ip4 value or hostname
+read -p "Enter IP address or hostname (leave blank to auto-detect - if you're running on wsl you will need to manually enter your IP): " input_ip4
+if [ -z "$input_ip4" ]; then
+    export ip4=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+else
+    export ip4=$input_ip4
+fi
 
 # Run docker-compose detached, remove -d flag to see docker-compose output
 docker-compose up -d
