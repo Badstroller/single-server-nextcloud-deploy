f566c28750e961225f915b9cbad864358f1f4eff
Badstroller <spagnolodavid@gmail.com>
2023-12-20 14:43:04 -0600

    Update user prompt

--- nextcloud.sh
+++ nextcloud.sh
@@ -14,7 +14,7 @@ mkdir ./deployment
 touch ./deployment/redis-session.ini
 
 # Ask for ip4 value or hostname
-read -p "Enter IP address or hostname (leave blank to auto-detect - if you're running on wsl you will need to manually enter your IP): " input_ip4
+read -p "Enter IP address or hostname (leave blank to auto-detect - if you're running on wsl you will need to manually enter your host machine's IP): " input_ip4
 if [ -z "$input_ip4" ]; then
     export ip4=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 else
